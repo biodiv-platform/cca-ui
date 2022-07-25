5bc3ee9406c0e052e582fac0b845e536c227b7ba
Harsh Zalavadiya <harsh@strandls.com>
2022-07-25 13:50:36 +0530

    fix: :bug: tinymce not loading correctly

--- src/components/@core/wysiwyg-editor/index.tsx
+++ src/components/@core/wysiwyg-editor/index.tsx
@@ -1,4 +1,9 @@
+/* eslint-disable simple-import-sort/imports */
+import "tinymce/skins/ui/oxide/content.min.css";
+import "tinymce/skins/ui/oxide/skin.min.css";
+
 import "tinymce/tinymce";
+import "tinymce/models/dom/model";
 import "tinymce/icons/default";
 import "tinymce/plugins/code";
 import "tinymce/plugins/image";
@@ -8,7 +13,6 @@ import "tinymce/plugins/table";
 import "tinymce/themes/silver/theme";
 
 import { Editor } from "@tinymce/tinymce-react";
-import Head from "next/head";
 import React from "react";
 
 interface WYSIWYGEditorProps {
@@ -18,34 +22,20 @@ interface WYSIWYGEditorProps {
 
 export default function WYSIWYGEditor({ uploadHandler, ...props }: WYSIWYGEditorProps) {
   return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/tinymce@latest/skins/ui/oxide/content.min.css"
-          key="tinymce-0-oxide"
-        />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/tinymce@latest/skins/ui/oxide/skin.min.css"
-          key="tinymce-1-skin"
-        />
-      </Head>
-      <Editor
-        {...props}
-        init={{
-          skin: false,
-          width: "100%",
-          height: "300px",
-          relative_urls: false,
-          convert_urls: false,
-          plugins: ["link", "table", "code", "lists", uploadHandler ? "image" : "na"],
-          toolbar:
-            "undo redo | bold italic numlist bullist | alignleft aligncenter alignright alignjustify | link image table | code",
-          images_upload_handler: uploadHandler,
-          images_upload_base_path: "/"
-        }}
-      />
-    </>
+    <Editor
+      {...props}
+      init={{
+        skin: false,
+        width: "100%",
+        height: "300px",
+        relative_urls: false,
+        convert_urls: false,
+        plugins: ["link", "table", "code", "lists", uploadHandler ? "image" : "na"],
+        toolbar:
+          "undo redo | bold italic numlist bullist | alignleft aligncenter alignright alignjustify | link image table | code",
+        images_upload_handler: uploadHandler,
+        images_upload_base_path: "/"
+      }}
+    />
   );
 }
