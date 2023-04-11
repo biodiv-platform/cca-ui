e3c380ad3fdee6c843a81e17a1d872f146a7be08
abrahul <abrahul100@gmail.com>
2023-04-11 10:32:44 +0530

    rectified lint errors

--- src/icons/required.tsx
+++ src/icons/required.tsx
@@ -1,17 +1,18 @@
-
 import { createIcon } from "@chakra-ui/icon";
 import React from "react";
 
 const RequiredIcon = createIcon({
   displayName: "Required",
   path: (
-    <path d="M12 2V22M19.0711 4.92893L4.92893 19.0711M22 12H2M19.0711 19.0711L4.92893 4.92893" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path
+      d="M12 2V22M19.0711 4.92893L4.92893 19.0711M22 12H2M19.0711 19.0711L4.92893 4.92893"
+      stroke="#000000"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   ),
   viewBox: "0 -5 32 32"
 });
 
 export default RequiredIcon;
-
-
-
-
--- src/icons/summary.tsx
+++ src/icons/summary.tsx
@@ -4,7 +4,13 @@ import React from "react";
 const SummaryIcon = createIcon({
   displayName: "Number",
   path: (
-<path  d="M8 8H19M8 12H19M8 16H19M5 8V8.00999M5 12V12.01M5 16V16.01" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path
+      d="M8 8H19M8 12H19M8 16H19M5 8V8.00999M5 12V12.01M5 16V16.01"
+      stroke="#000000"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   ),
   viewBox: "0 6 20 12"
 });
