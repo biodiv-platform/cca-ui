9130d7f88829a54a6cc1a348ce89408b6866ed8d
arunasd463 <arunbala.balasubramani@strandls.com>
2023-12-11 10:37:14 +0530

    update: :memo: added activity lable and types

--- src/components/@core/activity/activity-types.ts
+++ src/components/@core/activity/activity-types.ts
@@ -3,11 +3,14 @@ export default {
   FIELD_UPDATED: "Field Updated",
   FIELD_CREATED: "Field Created",
   FIELD_DELETED: "Field Deleted",
+  FOLLOWER_ADDED: "Follower added",
+  FOLLOWER_REMOVED: "Follower removed",
   DATA_UPDATED: "Data Updated",
   DATA_CREATED: "Data Created",
   DATA_COMMENTED: "Data comment",
   PERMISSION_REMOVED: "Permission removed",
   PERMISSION_ADDED: "Permission added",
-  FOLLOWER_ADDED: "Follower added",
-  FOLLOWER_REMOVED: "Follower removed"
+  POSTED_RESOURCE: "Posted resource",
+  REMOVED_RESORUCE: "Removed resoruce",
+  UNFEATURED: "UnFeatured"
 };
--- src/components/@core/activity/content-box.tsx
+++ src/components/@core/activity/content-box.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
+import BlueLink from "@components/@core/blue-link";
 import { ShowActivityIbp } from "@interfaces/activity";
 import React from "react";
 
@@ -9,6 +10,15 @@ const ContentBox = ({ activity }: { activity: ShowActivityIbp }) => {
   const at = activity.activityIbp?.activityType;
 
   switch (at) {
+    case ACTIVITY_TYPE.POSTED_RESOURCE:
+    case ACTIVITY_TYPE.REMOVED_RESORUCE:
+      const ad1 = JSON.parse(activity.activityIbp?.activityDescription || "{}");
+      return (
+        <Box>
+          <BlueLink href={ad1?.webAddress}>{ad1?.userGroupName}</BlueLink>
+          {at !== ACTIVITY_TYPE.UNFEATURED && <Text as="p">{ad1?.featured}</Text>}
+        </Box>
+      );
     case ACTIVITY_TYPE.ADDED_A_COMMENT:
     case ACTIVITY_TYPE.PERMISSION_ADDED:
     case ACTIVITY_TYPE.PERMISSION_REMOVED:
