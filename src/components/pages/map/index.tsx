17f8b27d0d9de212a685d3a6063b6f7a745670b7
logan1x <sharmakhushal78@gmail.com>
2024-06-11 18:00:44 +0530

    feat ✨ : basic markers

--- src/components/pages/map/index.tsx
+++ src/components/pages/map/index.tsx
@@ -1,9 +1,11 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+
 import { Box, useToast } from "@chakra-ui/react";
 import ExternalBlueLink from "@components/@core/blue-link/external";
 import SITE_CONFIG from "@configs/site-config";
 import useGlobalState from "@hooks/use-global-state";
 import { Role } from "@interfaces/custom";
-//import { axGetObservationMapData } from "@services/observation.service";
+import { axSearchMapCCAData } from "@services/cca.service";
 import { ENDPOINT, isBrowser } from "@static/constants";
 import { hasAccess } from "@utils/auth";
 import { getMapCenter } from "@utils/location";
@@ -29,9 +31,7 @@ export default function MapPageComponent({ defaultLayers }) {
   const isAdmin = hasAccess([Role.Admin]);
   const [selectedLayers, setSelectedLayers] = useState(defaultLayers);
 
-  //   const onObservationGridHover = ({ feature }) => (
-  //     <div>{feature?.properties?.count} Observations</div>
-  //   );
+  const [locationArr, setLocationArr] = useState<any>(null);
 
   const handleOnDownload = async (layerId) => {
     console.debug(`Layer download requested ${layerId}`);
@@ -52,67 +52,55 @@ export default function MapPageComponent({ defaultLayers }) {
     });
   };
 
-//   const fetchGridData = async (geoProps) => {
-//     const params = {
-//       ...geoProps,
-//       view: "map",
-//       geoField: "location"
-//     };
-
-//     const { data } = await axGetObservationMapData(params);
-//     return data;
-//   };
-
   useEffect(() => {
     if (isBrowser) {
       window.history.pushState("", "", `?${stringify({ layers: selectedLayers.toString() })}`);
     }
   }, [selectedLayers]);
 
+  useEffect(() => {
+    (async () => {
+      const { data } = await axSearchMapCCAData({
+        language: "en",
+        limit: 10,
+        offset: 0,
+        query: ""
+      });
+      setLocationArr(data);
+    })();
+  }, []);
+
   return (
     <Box height="calc(100vh - var(--heading-height))" overflow="hidden" position="relative">
-      <NakshaMapboxList
-        lang={lang}
-        defaultViewState={defaultViewState}
-        loadToC={true}
-        showToC={true}
-        selectedLayers={defaultLayers}
-        onSelectedLayersChange={setSelectedLayers}
-        nakshaEndpointToken={`Bearer ${user.accessToken}`}
-        mapboxAccessToken={SITE_CONFIG.TOKENS.MAPBOX}
-        nakshaApiEndpoint={ENDPOINT.NAKSHA}
-        onLayerDownload={handleOnDownload}
-        canLayerShare={true}
-        geoserver={{
-          endpoint: ENDPOINT.GEOSERVER,
-          store: SITE_CONFIG.GEOSERVER.STORE,
-          workspace: SITE_CONFIG.GEOSERVER.WORKSPACE
-        }}
-        managePublishing={isAdmin}
-        // layers={
-        //   SITE_CONFIG.MAP.DEFAULT_LAYERS
-        //     ? [
-        //         {
-        //           id: "global-observations",
-        //           title: "Observations",
-        //           description: "All observations from this portal",
-        //           attribution: "Portal and Contributors",
-        //           tags: ["Global", "Observations"],
-        //           isAdded: false,
-        //           source: { type: "grid", fetcher: fetchGridData },
-        //           onHover: onObservationGridHover,
-        //           data: {
-        //             index: "extended_observation",
-        //             type: "extended_records",
-        //             geoField: "location",
-        //             summaryColumn: ["count"],
-        //             propertyMap: { count: "Count" }
-        //           }
-        //         }
-        //       ]
-        //     : []
-        // }
-      />
+      {locationArr && (
+        <NakshaMapboxList
+          lang={lang}
+          markers={
+            locationArr &&
+            locationArr.map((loc) => ({
+              latitude: loc.lat,
+              longitude: loc.lng,
+              colorHex: "red"
+            }))
+          }
+          defaultViewState={defaultViewState}
+          loadToC={true}
+          showToC={true}
+          selectedLayers={defaultLayers}
+          onSelectedLayersChange={setSelectedLayers}
+          nakshaEndpointToken={`Bearer ${user.accessToken}`}
+          mapboxAccessToken={SITE_CONFIG.TOKENS.MAPBOX}
+          nakshaApiEndpoint={ENDPOINT.NAKSHA}
+          onLayerDownload={handleOnDownload}
+          canLayerShare={true}
+          geoserver={{
+            endpoint: ENDPOINT.GEOSERVER,
+            store: SITE_CONFIG.GEOSERVER.STORE,
+            workspace: SITE_CONFIG.GEOSERVER.WORKSPACE
+          }}
+          managePublishing={isAdmin}
+        />
+      )}
     </Box>
   );
 }
