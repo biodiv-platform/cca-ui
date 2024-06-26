3a0ecd95778af16dda1dcf552a6b477abb72a825
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2024-06-26 08:52:30 +0530

    test: :alembic: testing ggogle analytics without tagmanager

--- src/components/@core/container/metadata.tsx
+++ src/components/@core/container/metadata.tsx
@@ -1,34 +1,63 @@
 import SITE_CONFIG from "@configs/site-config";
+import useGlobalState from "@hooks/use-global-state";
+import { isBrowser, RESOURCE_SIZE } from "@static/constants";
+// import { CACHE_WHITELIST, removeCache } from "@utils/auth";
+// import { subscribeToPushNotification } from "@utils/user";
+import { getManifestURL } from "@utils/userGroup";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import Script from "next/script";
-import React from "react";
+import { DefaultSeo } from "next-seo";
+import useTranslation from "next-translate/useTranslation";
+import React, { useMemo } from "react";
 
 export default function Metadata() {
+  const router = useRouter();
+  const { currentGroup } = useGlobalState();
+  const canonical = SITE_CONFIG.SITE.URL + router.asPath;
+  const { lang } = useTranslation();
+  const manifestURL = useMemo(() => getManifestURL(currentGroup), [currentGroup]);
+
   return (
     <>
+      <DefaultSeo
+        title={currentGroup?.name}
+        canonical={canonical}
+        description={SITE_CONFIG.SITE.DESCRIPTION}
+        openGraph={{
+          type: "website",
+          locale: lang,
+          url: canonical,
+          title: currentGroup?.name,
+          site_name: currentGroup?.name,
+          description: SITE_CONFIG.SITE.DESCRIPTION
+        }}
+        twitter={{
+          handle: SITE_CONFIG.FOOTER.SOCIAL.TWITTER.HANDLE,
+          site: SITE_CONFIG.FOOTER.SOCIAL.TWITTER.HANDLE,
+          cardType: "summary_large_image"
+        }}
+      />
+      <Head>
+        <link rel="apple-touch-icon" href={currentGroup?.icon + RESOURCE_SIZE.APPLE_TOUCH} />
+        <link rel="manifest" href={manifestURL} />
+      </Head>
       {SITE_CONFIG.TRACKING.ENABLED && (
         <>
-          {/* GTM Head Tag */}
           <Script
             strategy="afterInteractive"
-            id="gtm-head"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${SITE_CONFIG.TRACKING.GTM_ID}');
-              `
-            }}
+            src={`https://www.googletagmanager.com/gtag/js?id=${SITE_CONFIG.TRACKING.GA_ID}`}
           />
-
-          {/* GTM Body Tag */}
-          <noscript
+          <Script
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `
-                <iframe src="https://www.googletagmanager.com/ns.html?id=${SITE_CONFIG.TRACKING.GTM_ID}"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>
-              `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', ${SITE_CONFIG.TRACKING.GA_ID});
+        `
             }}
           />
         </>
