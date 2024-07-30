a4badfed0a3b3af7b60e69c0c7b533f52bd70c2f
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2024-07-30 20:36:09 +0530

    refactor: :recycle: optimised cca multimedia code

--- src/components/form/multimedia.tsx
+++ src/components/form/multimedia.tsx
@@ -2,36 +2,44 @@ import { Box, Icon, Image } from "@chakra-ui/react";
 import AudioIcon from "@icons/audio";
 import CrossIcon from "@icons/cross";
 import VideoIcon from "@icons/video";
+import { ResourceType } from "@interfaces/custom";
+import { getSupportedTypeByPath } from "@utils/media";
 import React from "react";
 
+const commonBoxProps = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "md",
+  bg: "gray.200"
+};
+
 export const getMediaElementFromPath = (path) => {
-  const ext = path.split(".").pop().toLowerCase();
-  const isVideo = ["mp4", "webm", "mov"].includes(ext);
-  const isAudio = ["mp3", "wav", "ogg"].includes(ext);
-  const isImage = ["jpg", "jpeg", "png"].includes(ext);
+  const mediaType = getSupportedTypeByPath(path);
 
-  if (isImage) {
-    return <Image src={path} alt={path} objectFit="cover" borderRadius="md" />;
+  switch (mediaType) {
+    case ResourceType.Image:
+      return <Image src={path} alt={path} objectFit="cover" borderRadius="md" />;
+
+    case ResourceType.Video:
+      return (
+        <Box {...commonBoxProps}>
+          <Icon as={VideoIcon} boxSize={12} />
+        </Box>
+      );
+
+    case ResourceType.Audio:
+      return (
+        <Box {...commonBoxProps}>
+          <Icon as={AudioIcon} boxSize={12} />
+        </Box>
+      );
+
+    default:
+      return (
+        <Box {...commonBoxProps}>
+          <Icon as={CrossIcon} boxSize={12} />
+        </Box>
+      );
   }
-
-  if (isVideo || isAudio) {
-    const IconComponent = isVideo ? VideoIcon : AudioIcon;
-    return (
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        borderRadius="md"
-        bg="gray.200"
-      >
-        <Icon as={IconComponent} boxSize={12} />
-      </Box>
-    );
-  }
-
-  return (
-    <Box display="flex" alignItems="center" justifyContent="center" borderRadius="md" bg="gray.200">
-      <Icon as={CrossIcon} boxSize={12} />
-    </Box>
-  );
 };
