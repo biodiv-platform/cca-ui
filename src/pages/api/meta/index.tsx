9c63d535c24594300804d2115228cb7ba21d4c04
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2024-11-12 10:32:08 +0530

    test: :alembic: testing youtube og with open-graph-scraper

--- src/pages/api/meta/index.tsx
+++ src/pages/api/meta/index.tsx
@@ -1,12 +1,32 @@
-import { fetch } from "fetch-opengraph";
+// import { fetch } from "fetch-opengraph";
+
+// export default async function handler(req, res) {
+//   const data = await fetch(req.query.url);
+
+//   res.json({
+//     href: req.query.url,
+//     title: data.title,
+//     image: data.image,
+//     description: data.description,
+//   });
+// }
+
+import ogs from "open-graph-scraper";
 
 export default async function handler(req, res) {
-  const data = await fetch(req.query.url);
+  const options = { url: req.query.url };
+  const { result, error } = await ogs(options);
 
-  res.json({
-    href: req.query.url,
-    title: data.title,
-    image: data.image,
-    description: data.description,
-  });
+  if (error) {
+    res.status(500).json({ error: "Failed to fetch Open Graph metadata" });
+  } else {
+    const imageUrl = Array.isArray(result.ogImage) ? result.ogImage[0]?.url : result.ogImage?.url;
+
+    res.json({
+      href: req.query.url,
+      title: result.ogTitle,
+      image: imageUrl || null,
+      description: result.ogDescription || null
+    });
+  }
 }
