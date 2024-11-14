e364d0dec7bdcebd0409c4a071b6b484f2c8a8a2
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2024-11-14 10:40:20 +0530

    test: :alembic: testing youtueb og tags

--- src/pages/api/meta/index.tsx
+++ src/pages/api/meta/index.tsx
@@ -1,21 +1,21 @@
-import ogs from "open-graph-scraper";
+import { fetch } from "fetch-opengraph";
 
 export default async function handler(req, res) {
-  const options = { url: req.query.url };
-  const { result, error } = await ogs(options);
-
-  if (error) {
-    res.status(500).json({ error: "Failed to fetch Open Graph metadata" });
-  } else {
-    const imageUrl = Array.isArray((result as any).ogImage)
-      ? (result as any).ogImage[0]?.url
-      : (result as any).ogImage?.url;
+  try {
+    const data = await fetch(req.query.url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+      }
+    });
 
     res.json({
       href: req.query.url,
-      title: (result as any).ogTitle,
-      image: imageUrl || null,
-      description: (result as any).ogDescription || null
+      title: data.title,
+      image: data.image,
+      description: data.description
     });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch OG data" });
   }
 }
