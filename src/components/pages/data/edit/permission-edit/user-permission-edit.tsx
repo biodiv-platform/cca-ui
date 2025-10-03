abb54066e4d756aadd92f8750ff988661070b36a
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2025-10-03 11:47:27 +0530

    fix: :bug: fixed cca data contributor dialog

--- src/components/pages/data/edit/permission-edit/user-permission-edit.tsx
+++ src/components/pages/data/edit/permission-edit/user-permission-edit.tsx
@@ -1,4 +1,4 @@
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Dialog, Input } from "@chakra-ui/react";
 import { SelectAsyncInputField } from "@components/form/select-async";
 import { SubmitButton } from "@components/form/submit-button";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -10,15 +10,6 @@ import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import * as Yup from "yup";
 
-import {
-  DialogBackdrop,
-  DialogBody,
-  DialogCloseTrigger,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogRoot
-} from "@/components/ui/dialog";
 import { Field } from "@/components/ui/field";
 
 import useTemplateResponseEdit from "../use-template-response-edit";
@@ -57,37 +48,40 @@ export default function UserPermissionEditor({ initialUsers, author }) {
   const handleOnClose = () => setIsEdit(false);
 
   return (
-    <DialogRoot open={isEdit} onOpenChange={handleOnClose}>
-      <DialogBackdrop />
-      <DialogContent>
-        <FormProvider {...hForm}>
-          <form onSubmit={hForm.handleSubmit(handleFormSubmit)} className="fade">
-            <DialogHeader>{t("form:permission.title")}</DialogHeader>
-            <DialogCloseTrigger />
-            <DialogBody>
-              <Field mb={4}>
-                <Field label={t("common:owner")} />
-                <Input type="text" disabled={true} value={author.name} />
-              </Field>
-              <SelectAsyncInputField
-                name="allowedUsers"
-                label={t("form:contributors_for_document")}
-                placeholder={t("form:contributors_for_document")}
-                onQuery={onUserQuery}
-                multiple={true}
-                isCreatable={false}
-                isClearable={false}
-              />
-            </DialogBody>
-            <DialogFooter>
-              <Button mr={3} onClick={handleOnClose} variant={"subtle"}>
-                {t("common:close")}
-              </Button>
-              <SubmitButton>{t("common:save")}</SubmitButton>
-            </DialogFooter>
-          </form>
-        </FormProvider>
-      </DialogContent>
-    </DialogRoot>
+    <Dialog.Root open={isEdit} onOpenChange={handleOnClose}>
+      <Dialog.Backdrop />
+      <Dialog.Positioner>
+        <Dialog.Content>
+          <FormProvider {...hForm}>
+            <form onSubmit={hForm.handleSubmit(handleFormSubmit)} className="fade">
+              <Dialog.Header>{t("form:permission.title")}</Dialog.Header>
+              <Dialog.CloseTrigger />
+              <Dialog.Body>
+                <Field mb={4}>
+                  <Field label={t("common:owner")} />
+                  <Input type="text" disabled={true} value={author.name} />
+                </Field>
+                <SelectAsyncInputField
+                  name="allowedUsers"
+                  label={t("form:contributors_for_document")}
+                  placeholder={t("form:contributors_for_document")}
+                  onQuery={onUserQuery}
+                  multiple={true}
+                  isCreatable={false}
+                  isClearable={false}
+                  portalled={false}
+                />
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button mr={3} onClick={handleOnClose} variant={"subtle"}>
+                  {t("common:close")}
+                </Button>
+                <SubmitButton>{t("common:save")}</SubmitButton>
+              </Dialog.Footer>
+            </form>
+          </FormProvider>
+        </Dialog.Content>
+      </Dialog.Positioner>
+    </Dialog.Root>
   );
 }
