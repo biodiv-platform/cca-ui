dbecd10d82409bffad4c39052bd037cf1485da42
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2025-10-09 16:47:33 +0530

    fix: :bug: template edit controlled edit fix

--- src/components/pages/template/edit/modals/update-field-modal/index.tsx
+++ src/components/pages/template/edit/modals/update-field-modal/index.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import useTemplate from "../../use-template";
 import UpdateFieldForm from "./form";
 import { Dialog, Portal } from "@chakra-ui/react";
 
 export default function UpdateFieldModal() {
   const { currentField, clearCurrentField } = useTemplate();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(Boolean(currentField));
+  }, [currentField]);
+
+  const handleOpenChange = (e) => {
+    setOpen(e.open);
+    if (!e.open) {
+      clearCurrentField();
+    }
+  };
 
   return (
-    <Dialog.Root size="xl" open={currentField} modal={false}>
+    <Dialog.Root size="xl" open={open} onOpenChange={handleOpenChange} lazyMount>
       <Portal>
         <Dialog.Backdrop />
         <Dialog.Positioner>
