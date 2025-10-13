da671ed983759de31d460e09d942589472c2cef7
arunasd463 <30496644+arunasd463@users.noreply.github.com>
2025-10-13 11:15:13 +0530

    Template edit fix (#68)
    * fix: :bug: template edit fix
    
    * fix: :bug: template edit controlled edit fix

--- src/components/pages/template/edit/modals/update-field-modal/index.tsx
+++ src/components/pages/template/edit/modals/update-field-modal/index.tsx
@@ -1,17 +1,33 @@
-import React from "react";
-
-import { DialogBackdrop, DialogRoot } from "@/components/ui/dialog";
-
+import React, { useState, useEffect } from "react";
 import useTemplate from "../../use-template";
 import UpdateFieldForm from "./form";
+import { Dialog, Portal } from "@chakra-ui/react";
 
 export default function UpdateFieldModal() {
   const { currentField, clearCurrentField } = useTemplate();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(Boolean(currentField));
+  }, [currentField]);
+
+  const handleOpenChange = (e) => {
+    setOpen(e.open);
+    if (!e.open) {
+      clearCurrentField();
+    }
+  };
 
   return (
-    <DialogRoot open={currentField} onOpenChange={clearCurrentField} size="xl">
-      <DialogBackdrop />
-      {currentField && <UpdateFieldForm defaultValues={currentField} onClose={clearCurrentField} />}
-    </DialogRoot>
+    <Dialog.Root size="xl" open={open} onOpenChange={handleOpenChange} lazyMount>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          {currentField && (
+            <UpdateFieldForm defaultValues={currentField} onClose={clearCurrentField} />
+          )}
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
   );
 }
